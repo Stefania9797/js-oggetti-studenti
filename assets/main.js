c8a51cf182c36dee0be09abb9ab68ff813068617
Stefania9797 <stefaniamastrel@gmail.com>
2021-05-07 15:14:54 +0200

    js completato

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,3 @@
-/* 1 Creare un oggetto che descriva uno studente, con le seguenti proprietà:
-nome,
-cognome
-età.
-2 Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-3 Creare un array di oggetti di studenti.
-4 Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-5 Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.*/
-
 //1 Oggetto studente
 var studente = {
     "nome": "Stefania",
@@ -16,7 +7,7 @@ var studente = {
 console.log(studente);
 // 2 Stampare su pagina proprietà oggetto tramite for in
 for(var key in studente){
-    console.log(key);
+    console.log(studente[key]);
     document.getElementById("studente").innerHTML+="<li>"+studente[key]+"</li>";
 }
 //3 Creare un array di oggetti di studenti.
